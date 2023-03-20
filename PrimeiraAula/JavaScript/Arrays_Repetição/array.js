9e9f4c1b5bb073d26a240154c8616483a6f043f0
GxVitor <gabrielviphg@hotmail.com>
2023-03-19 21:37:49 -0300

    Aulas

---
+++ PrimeiraAula/JavaScript/Arrays_Repetição/array.js
@@ -0,0 +1,10 @@
+
+// [] quer dizer que é uma lista, começa do 0 até quando precisar
+// para pedir a posição chamar a variavel lista[3] e a posição
+
+const alunos = ['vitor', 'joao', 'maria', 'leo'];
+
+//push adiciona na list na ultima posição
+alunos.push("Ranan");
+
+console.log(alunos);
