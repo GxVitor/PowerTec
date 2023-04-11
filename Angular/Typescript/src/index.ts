afe1312049cc502c2aa46551701f1f4ac3e098ae
GxVitor <gabrielviphg@hotmail.com>
2023-04-10 21:23:22 -0300

    TypeScript

---
+++ Angular/Typescript/src/index.ts
@@ -0,0 +1,22 @@
+
+
+// tipos Primitivos
+// boolean = verdadeiro ou falso
+// number = numeros
+// string = text
+
+    let ligado: boolean = false;
+    let nome: string = "Vitor";
+    let idade: number = 20;
+
+// null
+// undefined
+
+    let nulo: null = null;
+    let indefinido: undefined = undefined;
+
+//any
+//void
+
+    let qualquercoisa: any = "tudo, 90, boolean, number";
+    let vaziu:void
